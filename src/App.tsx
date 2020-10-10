19b1ddbc0d56b408e8c2405d6e3dda29a44ecd6d
Adam Berg <adam@dubsado.com>
2020-10-10 12:44:45 -0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from "react-three-fiber";
+import { Color, Mesh, Vector3 } from "three";
 
-function App() {
+type Props = {
+  position: Vector3;
+};
+
+function Box(props: Props) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef<Mesh>();
+
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame(() => {
+    const m = mesh.current as Mesh;
+    m.rotation.x = m.rotation.y += 0.01;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onClick={(e) => setActive(!active)}
+      onPointerOver={(e) => setHover(true)}
+      onPointerOut={(e) => setHover(false)}
+    >
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.4} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} color={new Color("red")}/>
+      <pointLight position={[-10, -10, -10]} />
+      <Box position={new Vector3(-1.2, 0, 0)} />
+      <Box position={new Vector3(1.2, 0, 0)} />
+    </Canvas>
+  );
+}
