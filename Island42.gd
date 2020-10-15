2c49b03a3d4022b049ed83f78e8d467f72c17f5c
Adam Berg <adam@dubsado.com>
2020-10-14 20:11:28 -0700

    Add "win" condition

--- Island42.gd
+++ Island42.gd
@@ -2,6 +2,7 @@ extends Spatial
 
 var water
 var player
+var speed = 0.1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,10 +12,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var speed = 0.1
 	water.translate(Vector3(0, speed * delta, 0))
 
 
 func _on_water_body_entered(body):
 	if body == player:
 		get_tree().change_scene("res://GameOver.tscn")
+
+
+func _on_Palm_body_entered(body):
+	if body == player:
+		speed = -0.1
+
